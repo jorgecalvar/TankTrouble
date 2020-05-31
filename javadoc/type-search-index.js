0a8d7bced39a9d9622a5713fcd2db6c4a73c7860
Jorge Calvar <calvarjorge@gmail.com>
2020-05-31 14:50:06 +0200

    Creado javadocs.

--- javadoc/type-search-index.js
+++ javadoc/type-search-index.js
@@ -13,28 +13,31 @@ typeSearchIndex = [{"l": "All Classes", "url": "allclasses-index.html"}, {
 }, {"p": "tanktrouble.ui", "l": "CreateWindow"}, {
     "p": "tanktrouble.labcreator",
     "l": "CreatorCanvas"
-}, {"p": "tanktrouble.ui", "l": "Dibujo"}, {"p": "tanktrouble.labcreator", "l": "Dot"}, {
-    "p": "tanktrouble.reflection",
-    "l": "Environment"
-}, {"p": "tanktrouble.ui", "l": "GameWindow"}, {
+}, {"p": "tanktrouble.reflection", "l": "Dibujo"}, {
+    "p": "tanktrouble.labcreator",
+    "l": "Dot"
+}, {"p": "tanktrouble.reflection", "l": "Environment"}, {
     "p": "tanktrouble.ui",
-    "l": "InicioWindow"
-}, {"p": "tanktrouble.control", "l": "InternetController"}, {
-    "p": "tanktrouble.reflection",
-    "l": "Lab"
-}, {"p": "tanktrouble.labcreator", "l": "LabManager"}, {
-    "p": "tanktrouble.reflection",
-    "l": "Movible"
-}, {"p": "tanktrouble.ui", "l": "OptionsWindow"}, {
-    "p": "tanktrouble.reflection",
-    "l": "Pared"
-}, {"p": "tanktrouble.reflection", "l": "Pintable"}, {
-    "p": "tanktrouble.net",
-    "l": "Servidor"
-}, {"p": "tanktrouble.misc", "l": "Sonido"}, {"p": "tanktrouble.control", "l": "Speeder"}, {
-    "p": "tanktrouble.misc",
-    "l": "Styler"
-}, {"p": "tanktrouble.reflection", "l": "Tanque"}, {
+    "l": "GameWindow"
+}, {"p": "tanktrouble.ui", "l": "InicioWindow"}, {
     "p": "tanktrouble.control",
-    "l": "TanqueController"
-}, {"p": "tanktrouble.control", "l": "UserController"}, {"p": "tanktrouble.misc", "l": "Util"}]
+    "l": "InternetController"
+}, {"p": "tanktrouble.reflection", "l": "Lab"}, {
+    "p": "tanktrouble.labcreator",
+    "l": "LabManager"
+}, {"p": "tanktrouble.reflection", "l": "Movible"}, {
+    "p": "tanktrouble.ui",
+    "l": "OptionsWindow"
+}, {"p": "tanktrouble.reflection", "l": "Pared"}, {
+    "p": "tanktrouble.reflection",
+    "l": "Pintable"
+}, {"p": "tanktrouble.net", "l": "Servidor"}, {"p": "tanktrouble.misc", "l": "Sonido"}, {
+    "p": "tanktrouble.control",
+    "l": "Speeder"
+}, {"p": "tanktrouble.misc", "l": "Styler"}, {
+    "p": "tanktrouble.reflection",
+    "l": "Tanque"
+}, {"p": "tanktrouble.control", "l": "TanqueController"}, {
+    "p": "tanktrouble.control",
+    "l": "UserController"
+}, {"p": "tanktrouble.misc", "l": "Util"}]
